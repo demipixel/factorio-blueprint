326e77cda18f74bc76b1c09cd2ff4957f00bf4d7
DemiPixel <luke5227@gmail.com>
2016-06-23 01:52:43 -0700

    Added more entities that are in vanilla

--- defaultentities.js
+++ defaultentities.js
@@ -5,21 +5,154 @@ var virtual = 'virtual';
 module.exports = { // ADD MORE (vanilla) AS YOU PLEASE (or modded if it's just for you)! 
                    // Somebody will probably automate the gathering of this data soon...
 
+  tank: {
+    type: item
+  },
+  car: {
+    type: item
+  },
+  cargo_wagon: {
+    type: item
+  },
+  diesel_locomotive: {
+    type: item
+  },
+
   basic_armor: {
     type: item
   },
-  'basic_bullet_magazine': {
+  basic_bullet_magazine: {
     type: item
   },
-  'heavy_armor':{
+  heavy_armor: {
+    type: item
+  },
+  basic_grenade: {
+    type: item
+  },
+  flame_thrower: {
+    type: item
+  },
+  flame_thrower_ammo: {
+    type: item
+  },
+  rocket_launcher: {
+    type: item
+  },
+  rocket: {
+    type: item
+  },
+  explosive_rocket: {
+    type: item
+  },
+  combat_shotgun: {
+    type: item
+  },
+  shotgun: {
+    type: item
+  },
+  shotgun_shell: {
+    type: item
+  },
+  piercing_shotgun_shell: {
+    type: item
+  },
+  submachine_gun: {
+    type: item
+  },
+  pistol: {
+    type: item
+  },
+  bullet_magazine: {
+    type: item
+  },
+  piercing_bullet_magazine: {
+    type: item
+  },
+  cannon_shell: {
+    type: item
+  },
+  explosive_cannon_shell: {
     type: item
   },
 
+  destroyer_capsule: {
+    type: item
+  },
+  distractor_capsule: {
+    type: item
+  },
+  defender_capsule: {
+    type: item
+  },
+  slowdown_capsule: {
+    type: item
+  },
+  poison_capsule: {
+    type: item
+  },
 
+  stone_brick: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  concrete: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+
+  iron_axe: {
+    type: item
+  },
+  steel_axe: {
+    type: item
+  },
+  repair_pack: {
+    type: item
+  },
+  blueprint: {
+    type: item
+  },
+  deconstruction_planner: {
+    type: item
+  },
+
+  copper_cable: {
+    type: item
+  },
+  red_wire: {
+    type: item
+  },
+  green_wire: {
+    type: item
+  },
+
+  basic_beacon: {
+    type: item,
+    width: 3,
+    height: 3
+  },
+  small_electric_pole: {
+    type: item,
+    width: 1,
+    height: 1
+  },
   medium_electric_pole: {
     type: item,
     width: 1,
     height: 1
+  },
+  substation: {
+    type: item,
+    width: 2,
+    height: 2
+  },
+  big_electric_pole: {
+    type: item,
+    width: 2,
+    height: 2
   },
   offshore_pump: {
     type: item,
@@ -45,6 +178,11 @@ module.exports = { // ADD MORE (vanilla) AS YOU PLEASE (or modded if it's just f
     type: item,
     width: 1,
     height: 2
+  },
+  constant_combinator: {
+    type: item,
+    width: 1,
+    height: 1
   },
   
   basic_underground_belt: {
@@ -83,120 +221,272 @@ module.exports = { // ADD MORE (vanilla) AS YOU PLEASE (or modded if it's just f
     height: 1
   },
   fast_transport_belt: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   express_transport_belt: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
+  },
+  basic_transport_belt_to_ground: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  fast_transport_belt_to_ground: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  express_transport_belt_to_ground: {
+    type: item,
+    width: 1,
+    height: 1
   },
   assembling_machine_1: {
-    type:item,
-    width:3,
-    height:3
+    type: item,
+    width: 3,
+    height: 3
   },
   assembling_machine_2: {
-    type:item,
-    width:3,
-    height:3
+    type: item,
+    width: 3,
+    height: 3
   },
   assembling_machine_3: {
-    type:item,
-    width:3,
-    height:3
+    type: item,
+    width: 3,
+    height: 3
+  },
+  wooden_chest: {
+    type: item,
+    width: 1,
+    height: 1
   },
   iron_chest: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   steel_chest: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   smart_chest: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   logistic_chest_passive_provider: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   logistic_chest_active_provider: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   logistic_chest_storage: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   logistic_chest_requester: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
+  },
+  storage_tank: {
+    type: item,
+    width: 3,
+    height: 3
+  },
+  burner_inserter: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  basic_inserter: {
+    type: item,
+    width: 1,
+    height: 1
   },
   long_handed_inserter: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   fast_inserter: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   smart_inserter: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   gate: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   wall: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   straight_rail: {
-    type:item,
-    width:2,
-    height:2
+    type: item,
+    width: 2,
+    height: 2
   },
   curved_rail: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
   // Lets figure out curved rails later.
   land_mine: {
-    type:item,
-    width:1,
-    height:1
+    type: item,
+    width: 1,
+    height: 1
   },
-  train_station: { // pretty sure this is a 1.2x1.2 centered in a 2x2 square.
-    type:item,
-    width:2,
-    height:2
+  train_stop: { // pretty sure this is a 1.2x1.2 centered in a 2x2 square.
+    type: item,
+    width: 2,
+    height: 2
+  },
+  rail_signal: {
+    type: item,
+    width: 1,
+    height: 1
   },
   lab: {
-    type:item,
-    width:3,
-    height:3
+    type: item,
+    width: 3,
+    height: 3
   },
   rocket_silo: {
-    type:item,
-    width:10,
-    height:10 //unsure about these values, got them from code only (never counted it in game, but 10 sounds right.)
+    type: item,
+    width: 10,
+    height: 10 //unsure about these values, got them from code only (never counted it in game, but 10 sounds right.)
+  },
+  chemical_plant: {
+    type: item,
+    width: 3,
+    height: 3
+  },
+  oil_refinery: {
+    type: item,
+    width: 5,
+    height: 5
+  },
+  stone_furnace: {
+    type: item,
+    width: 2,
+    height: 2
+  },
+  steel_furnace: {
+    type: item,
+    width: 2,
+    height: 2
+  },
+  electric_furnace: {
+    type: item,
+    width: 3,
+    height: 3
+  },
+  pumpjack: {
+    type: item,
+    width: 3,
+    height: 3
+  },
+  burner_mining_drill: {
+    type: item,
+    width: 2,
+    height: 2
+  },
+  basic_mining_drill: {
+    type: item,
+    width: 3,
+    height: 3
+  },
+  small_pump: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  pipe: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  pipe_to_ground: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+
+
+  boiler: {
+    type: item,
+    width: 1,
+    height: 1
+  },
+  steam_engine: {
+    type: item,
+    width: 5
+    height: 3
+  },
+  basic_accumulator: {
+    type: item,
+    width: 2,
+    height: 2
+  },
+
+  roboport: {
+    type: item,
+    width: 4,
+    height: 4
+  },
+  construction_robot: {
+    type: item
+  },
+  logistic_robot: {
+    type: item
+  },
+
+  productivity_module: {
+    type: item
+  },
+  productivity_module_2: {
+    type: item
+  },
+  productivity_module_3: {
+    type: item
+  },
+  effectivity_module: {
+    type: item
+  },
+  effectivity_module_2: {
+    type: item
+  },
+  effectivity_module_3: {
+    type: item
+  },
+  speed_module: {
+    type: item
+  },
+  speed_module_2: {
+    type: item
+  },
+  speed_module_3: {
+    type: item
   },
   
 
@@ -223,6 +513,48 @@ module.exports = { // ADD MORE (vanilla) AS YOU PLEASE (or modded if it's just f
     type: virtual
   },
   signal_everything: {
+    type: virtual
+  },
+  signal_0: {
+    type: virtual,
+  },
+  signal_1: {
+    type: virtual
+  },
+  signal_2: {
+    type: virtual
+  },
+  signal_3: {
+    type: virtual
+  },
+  signal_4: {
+    type: virtual
+  },
+  signal_5: {
+    type: virtual
+  },
+  signal_6: {
+    type: virtual
+  },
+  signal_7: {
+    type: virtual
+  },
+  signal_8: {
+    type: virtual
+  },
+  signal_9: {
+    type: virtual
+  },
+  signal_blue: {
+    type: virtual
+  },
+  signal_red: {
+    type: virtual
+  },
+  signal_green: {
+    type: virtual
+  },
+  signal_yellow: {
     type: virtual
   }
 };
