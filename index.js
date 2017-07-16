2bbf1133f95829708f729a22a10e32d59a2b5d85
Unknown <game_stailer94@hotmail.de>
2017-07-16 17:31:31 +0200

    to book encode fix

--- index.js
+++ index.js
@@ -340,7 +340,7 @@ Blueprint.toBook = function(Blueprints,orgBook){
     let obj = util.decode(orgBook);
 
     obj.blueprint_book.blueprints = Blueprints;
-    return obj.encode();
+    return utils.encode(obj);
 }
 
 Blueprint.isBook = function(string){
