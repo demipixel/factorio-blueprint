d37461e810d4f3e1619bb4f50456b443dedcf5e6
Ryan Davis <ryepup@gmail.com>
2017-11-30 01:34:49 -0500

    Include a UMD bundle (#29)
    Use webpack + babel to make a browser-friendly UMD bundle before
    publishing the package to npm.
    
    closes #28

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: './dist/factorio-blueprint.min.js',
+        library: 'Blueprint',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
